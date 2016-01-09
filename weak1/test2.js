d97b91084f5f425610bffa81d939047432c74987
minseokism <lagender@gmail.com>
2016-01-09 15:50:54 +0900

    오늘 마지막 커밋

--- weak1/test2.js
+++ weak1/test2.js
@@ -1,1 +1,29 @@
-console.log('test2');
+console.log(sum(4));
+var sum=0;
+function sum(num){
+	return num+num;
+};
+console.log(sum);
+
+/*
+	순서
+	var sum;
+	function sum(num){
+	return num+num;
+	} ->sum 은 함수로 변경
+	console.log(sum(4));
+	sum=0; -> sum은 숫자 0 으로 변경
+	console.log(sum);
+};
+
+*/
+
+
+console.log(sum2);
+var sym2=0;
+function sum2(num){
+	return num+num;
+};
+console.log(sum(4))
+
+
