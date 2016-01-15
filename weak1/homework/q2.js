9e6448c4db3a8017b9e26d943b487cc1b5540c35
minseokism <lagender@gmail.com>
2016-01-16 00:18:48 +0900

    문제 2

---
+++ weak1/homework/q2.js
@@ -0,0 +1,16 @@
+function sumLineNum(input){
+
+	var hap=0;
+	var value=0;
+
+	while(!input==0){
+		value=input%10;
+		hap=hap+value;
+		input=parseInt(input/10);
+	}
+
+	console.log(hap);
+}
+
+sumLineNum(54321);
+sumLineNum(543);
