0945df15ddfc12b1b3965abe9023f3f1222743a6
Minseok Lee <lagender@gmail.com>
2016-01-20 16:18:20 +0900

    homework 1번 문제 푸는중

--- weak2/homework/q1.js
+++ weak2/homework/q1.js
@@ -1,1 +1,31 @@
-1
+/*
+ 2016-01-16 weak2 homework
+ 1. 아래와 같이 입력 받은 범위 내에서 미중복 랜덤 숫자를 출력시키는 프로그램을 작성하세요.
+ -  입력 받는 숫자는 2개입니다 (범위, 개수)
+ -  중복된 숫자는 출력되면 안됩니다.
+ -  Math.random 함수를 사용합니다. 
+ -  시간복잡도 제한은 n^2 이며, 문제를 풀게되면 n으로 할 수 있는 방법을 찾아보세요.
+ -  예시)  getRandomNum(20,5);
+ -        
+ -       20개의 숫자중 랜덤한 숫자 5개는
+ -       14
+ -       2
+ -       1
+ -       6
+ -       5
+ */
+
+function getRandomNum(x,y) {
+	var outputArray = [y];  								// random한 수를 받을 배열 생성
+	var count  = 0;
+	var output = "20개의 숫자중 랜덤한 숫자 5개는 ";
+	for(var i = 0 ; i < y ; i++) {					 
+		outputArray[i]=Math.floor(Math.random()*x+1); 	// random()함수를 통해 1~x 까지의 범위의 랜덤한 정수를 구한다.    
+		output+="\n"+outputArray[i];            
+	}
+
+
+	return output;
+};
+
+console.log(getRandomNum(20,5));
--- weak2/homework/q2.js
+++ weak2/homework/q2.js
@@ -1,1 +1,18 @@
-1
+/*
+ 2016-01-16 weak2 homework
+ 2. 아래와 같이 입력한 숫자를 역순으로 출력하는 프로그램을 작성하세요.
+ -  문자열, 배열, 기타 자료구조(리스트 등)사용 불가
+ -  예시) reverseNum(54321);
+ -       reverseNum(9876); 
+ -       
+ -       역순은 : 12345
+ -       역순은 : 6789
+ */
+
+function reverseNum(x){
+
+};
+
+reverseNum(54321);
+
+reverseNum(9876);
