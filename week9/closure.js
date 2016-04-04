5b11f58fe622caccf41bdf37594073d65ae3483b
Minseok Lee <lagender@gmail.com>
2016-04-04 15:32:30 +0900

    closure 숙제

--- week9/closure.js
+++ week9/closure.js
@@ -1,33 +1,37 @@
 /**
  * Created by jojoldu@gmail.com on 2016-03-12.
  */
-var buff = [
-    '나는 ',
-    '', //name
-    '이며,\n',
-    '', //city
-    '에 살고있습니다.\n나이는 ',
-    '', //age
-    '입니다.'
-];
+
 
 function getComplete(name, city, age){
+    var buff = [
+        '나는 ',
+        '', //name
+        '이며,\n',
+        '', //city
+        '에 살고있습니다.\n나이는 ',
+        '', //age
+        '입니다.'
+    ];
+
     if(name && city && age){
         buff[1] = name;
         buff[3] = city;
         buff[5] = age;
     }
-    return buff.join('');
+    return function(){
+        return buff.join('');
+    }
 }
 
 function showIntroduce(){
     var name = document.getElementById("name").value,
         city = document.getElementById("city").value,
         age = document.getElementById("age").value;
-    
-    document.getElementById("introduce").innerHTML = getComplete(name, city, age);
+        go = getComplete(name, city, age);
+    document.getElementById("introduce").innerHTML = go();
 }
 
 function showPrevIntroduce(){
-    document.getElementById("prevIntroduce").innerHTML = getComplete();
+    document.getElementById("prevIntroduce").innerHTML = go();
 }
